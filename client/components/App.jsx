a557036f968aae4b19c72b3fa5341bc53ac37a9a
Danny <Danny@Apples-iMac.local>
2021-11-13 16:47:36 +1300

    got sounds working with buttons

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,24 +1,18 @@
-import React, {useState, useEffect} from 'react'
-import {getGreeting} from '../apiClient'
+import React, { useState, useEffect } from 'react'
+import { playAudio } from '../apiClient'
 
 const App = () => {
-
-  const [greeting, setGreeting] = useState('')
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    getGreeting()
-      .then((greeting) => {
-        console.log(greeting)
-        setGreeting(greeting)
-      })
-  }, [count])
-
+  // const guitarStrings = ['firstE.wav', 'secondB.wav', 'thirdG.wav', 'fourthD', 'fifthA', 'sixthE.wav']
   return (
     <>
-    {count}
-    <h1>{greeting}</h1>
-    <button onClick={() => setCount(count + 1)}>Click</button>
+      <h1>Tune and Learn Your Guitar</h1>
+      <img className="guitar" src="./images/guitar.png" alt="a guitar and its tuning pegs"/><br></br>
+      <p>||--------------------------------------------------||<button id="sixthE" onClick={playAudio}>6 - E</button></p>
+      <p>||--------------------------------------------------||<button id="fifthA" onClick={playAudio}>5 - A</button></p>
+      <p>||--------------------------------------------------||<button id="fourthD" onClick={playAudio}>4 - D</button></p>
+      <p>||--------------------------------------------------||<button id="thirdG" onClick={playAudio}>3 - G</button></p>
+      <p>||--------------------------------------------------||<button id="secondB" onClick={playAudio}>2 - B</button></p>
+      <p>||--------------------------------------------------||<button id ="firstE" onClick={playAudio}>1 - E</button></p>
     </>
   )
 }
