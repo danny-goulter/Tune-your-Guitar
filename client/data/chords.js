331df3665b24d1423fa9a3049c233dd246bc9076
Danny <Danny@Apples-iMac.local>
2021-11-16 10:03:15 +1300

    building chord data

---
+++ client/data/chords.js
@@ -0,0 +1,32 @@
+export default {
+  'E Major': {
+    image: 'emajorpic.png',
+    imageplayed: 'emajorreal.jpg',
+    sound: 'emajor.wav'
+  },
+  'A Major': {
+    image: 'amajorpic.png',
+    imageplayed: 'amajorreal.jpg',
+    sound: 'amajor.wav'
+  },
+  'D Major': {
+    image: 'dmajorpic.png',
+    imageplayed: 'dmajorreal.jpg',
+    sound: 'dmajor.wav'
+  },
+  'C Major': {
+    image: 'cmajorpic.png',
+    imageplayed: 'cmajorreal.jpg',
+    sound: 'cmajor.wav'
+  },
+  'G Major': {
+    image: 'gmajorpic.png',
+    imageplayed: 'gmajorreal.jpg',
+    sound: 'gmajor.wav'
+  },
+  'F Major': {
+    image: 'fmajorpic.png',
+    imageplayed: 'fmajorreal.jpg',
+    sound: 'fmajor.wav'
+  }
+}
